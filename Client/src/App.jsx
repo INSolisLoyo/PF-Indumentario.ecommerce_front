fe589280e539d011c7763edec70e8102e793bc04
Nohemi Solis <mimisolisloyo@gmail.com>
2024-03-20 08:27:32 -0600

    Creacion de proyecto, paleta de colores y fuente

---
+++ Client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <div className='bg-primary/20 font-RedHat'>
+        Hola Front 💜
+      </div>
+    
+    </>
+  )
+}
+
+export default App
