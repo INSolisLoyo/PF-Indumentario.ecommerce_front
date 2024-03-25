439baf27fd1aec279ca02bd1d0a86ebd93a44afe
Nohemi Solis <mimisolisloyo@gmail.com>
2024-03-25 12:06:16 -0600

    Detail con galería funcional y componente DetailGallery agregado

--- Client/src/components/Detail/Detail.jsx
+++ Client/src/components/Detail/Detail.jsx
@@ -1,7 +1,8 @@
-import chroma from 'chroma-js';
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import chroma from 'chroma-js';
+import DetailGallery from "../DetailGallery/DetailGallery";
 import heart from '../../img/heart.png'
 
 
@@ -9,7 +10,6 @@ const Detail = () => {
 
     const { id } = useParams();
     const ENDPOINT = `http://localhost:3001/products/${id}`;
-    const [showedImage, setShowedImage] = useState('');
     const [counter, setCounter] = useState(1);
     const [item, setItem] = useState({
         name: '',
@@ -25,23 +25,39 @@ const Detail = () => {
 
         const arr = name.split(' ');
         const itemName = arr.slice(-3).join(' ');
-        const colours = colour.split(", ");
+        const materials = material.join(", ");
+
+        const galleryImages = images?.map( (image) => {
+            return {
+                original: image,
+                thumbnail: image
+            }
+        })
+
+        const newColors = [];
+
+        colour.forEach((col) => {
+            
+            if(col.includes(' ')){
+                const colors = col.split(' ');
+                newColors.push(colors[1])
+            } else {
+                newColors.push(col)
+            }
+            
+        });
 
         setItem(
             {
                 name: itemName,
                 price: price,
                 description: name,
-                images: images,
-                colours: colours,
-                material: material
+                images: galleryImages,
+                colours: newColors,
+                material: materials
             }
         )
         
-    }
-
-    const handleImage = (image) => {
-        setShowedImage(image)
     }
 
     const handleCounter = (op) => {
@@ -54,8 +70,6 @@ const Detail = () => {
         }
     }
 
-    
-
     useEffect( () => {
 
         const fetchData = async () => {
@@ -64,7 +78,6 @@ const Detail = () => {
                 
                 if(data){
                     cleaner(data);  
-                    setShowedImage(data.images[0])
                 }
                 else{
                     alert(`Doesn't exist any product with this id: ${id}`);
@@ -81,117 +94,92 @@ const Detail = () => {
 
     return (
 
-        <div className=" w-11/12 h-auto pt-20 mx-auto lg:w-3/5 lg:mt-10 font-RedHat">
+        <div className="w-11/12 h-auto pt-20 mx-auto lg:mt-10 font-RedHat flex flex-col gap-4 lg:flex-row">
 
-            <div className="bg-primary/10 rounded-2xl flex flex-col items-center md:gap-4 md:px-4 lg:flex-row lg:justify-around  lg:items-center">
+            {/* aquí va la gallery */}
+            <DetailGallery images={item.images} />
 
-                <picture>
-                    <img src={showedImage} alt="item"
-                        className="max-w-64 mt-4 lg:mt-0 rounded-2xl"
-                    />
-                </picture>
+            <div className="w-full md:h-full lg:w-2/5">
 
-                <div className="lg:max-w-96 bg-primary/20 my-10 rounded-2xl flex flex-col p-4">
+                <div className="bg-primary/10 rounded-2xl md:h-5/6 p-4 flex flex-col items-center md:p-8">
 
-                    <div className="flex justify-between">
-                        <h1 className="font-semibold text-sm tracking-widest">{item.name}</h1>
+                    <div className="w-full flex justify-between">
+                        <h1 className="font-semibold text-xl tracking-widest">{item.name}</h1>
                         <img src={heart} alt="heart" 
                             className="max-w-6 max-h-6"
                         />
                     </div>
-                    
-                    
-                    <div className="mt-5 flex justify-between">
-                        <p className="font-semibold text-2xl tracking-widest">${item.price}</p>
-                        <div className="flex gap-4 text-lg">
-                            <span className="bg-primary/50 hover:bg-primary rounded-2xl py-0.5 px-4" 
-                            onClick={() => handleCounter('-')}>-</span>
-                            <span>{counter}</span>
-                            <span className="bg-primary/50 hover:bg-primary rounded-2xl py-0.5 px-4" 
-                                onClick={() => handleCounter('+')}
-                            >+</span>
+
+                    <div className="md:w-full bg-primary/20 mt-4 rounded-2xl flex flex-col p-4">
+                       
+                        <div className="mt-5 flex justify-between">
+                            <p className="font-semibold text-2xl tracking-widest">${item.price}</p>
+                            <div className="flex gap-4 text-lg">
+                                <span className="bg-primary/50 hover:bg-primary rounded-2xl py-0.5 px-4" 
+                                onClick={() => handleCounter('-')}>-</span>
+                                <span>{counter}</span>
+                                <span className="bg-primary/50 hover:bg-primary rounded-2xl py-0.5 px-4" 
+                                    onClick={() => handleCounter('+')}
+                                >+</span>
+                            </div>
                         </div>
-                    </div>
 
-                    <p className="mt-2 text-sm"><span className="font-semibold">Description:</span> {item.description}</p>
-                    <p className="mt-2 text-sm"><span className="font-semibold">Material:</span> {item.material}</p>
+                        <p className="mt-2 text-sm"><span className="font-semibold">Description:</span> {item.description}</p>
+                        <p className="mt-2 text-sm"><span className="font-semibold">Material:</span> {item.material}</p>
 
-                    <div className="mt-2 flex flex-col gap-2 md:flex-row">
+                        <div className="mt-2 flex flex-col gap-2 md:flex-row">
 
-                        <div className="flex flex-col gap-2 mx-auto" id='colors'>
-                            
-                            <p className="text-center font-semibold text-lg">Color</p>
-                            <div className="flex gap-2 mx-auto justify-center">
+                            <div className="flex flex-col gap-2 mx-auto" id='colors'>
+                                
+                                <p className="text-center font-semibold text-lg">Color</p>
+                                <div className="flex gap-2 mx-auto justify-center">
 
-                                {
+                                    {
+                                        
+                                        item.colours?.map(  (color, index) => {
                                     
-                                     item.colours?.map(  (color, index) => {
-                                  
-                                         const hexColor =  chroma(color.toLowerCase()).hex();
-                                         console.log(hexColor);
-                                         const clas = `w-4 h-4 bg-[${hexColor}] rounded-full`
-                                         console.log(clas);
-                                         return (
-                                             <span 
-                                                 key={index}
-                                                 className={clas}
-                                             ></span>
-                                         )
-                                     })
-                                }
-                            
+                                            const hexColor =  chroma(color.toLowerCase()).hex();
+                                            
+                                            return (
+                                                <span 
+                                                    key={index}
+                                                    className={`w-4 h-4 bg-[${hexColor}] rounded-full`}
+                                                ></span>
+                                            )
+                                        })
+                                    }
+                                
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="flex flex-col gap-2 mx-auto">
+                            <div className="flex flex-col gap-2 mx-auto">
 
-                            <p className="text-center font-semibold text-lg">Size</p>
-                            <div className="flex mx-auto justify-center text-md">
-                                <span className=" hover:bg-primary rounded-2xl py-0.5 px-2 cursor-pointer">XS</span>
-                                <span className="hover:bg-primary rounded-2xl py-0.5 px-2 cursor-pointer">S</span>
-                                <span className="hover:bg-primary rounded-2xl py-0.5 px-2 cursor-pointer">M</span>
-                                <span className="hover:bg-primary rounded-2xl py-0.5 px-2 cursor-pointer">L</span>
-                                <span className="hover:bg-primary rounded-2xl py-0.5 px-2 cursor-pointer">XL</span>
+                                <p className="text-center font-semibold text-lg">Size</p>
+                                <div className="flex mx-auto justify-center text-md">
+                                    <span className=" hover:bg-primary rounded-2xl py-0.5 px-2 cursor-pointer">XS</span>
+                                    <span className="hover:bg-primary rounded-2xl py-0.5 px-2 cursor-pointer">S</span>
+                                    <span className="hover:bg-primary rounded-2xl py-0.5 px-2 cursor-pointer">M</span>
+                                    <span className="hover:bg-primary rounded-2xl py-0.5 px-2 cursor-pointer">L</span>
+                                    <span className="hover:bg-primary rounded-2xl py-0.5 px-2 cursor-pointer">XL</span>
+                                </div>
                             </div>
+
+
                         </div>
-
-
+                        
                     </div>
-                    
+
                 </div>
+
+                <div className="flex justify-center items-center pt-4 md:h-1/6">
+                
+                    <button 
+                    className="w-full h-8 bg-primary/70 hover:bg-primary rounded-2xl py-2 text-black md:w-2/4 lg:w-1/4">Go</button>
+
+                </div>
+
             </div>
 
-            <div className="flex flex-col items-center lg:flex-row lg:flex-wrap lg:justify-around pr-8">
-
-                <div className="mt-5 flex flex-wrap justify-center gap-4">
-
-                    {
-                        item.images?.map( (image) => {
-                            return (
-                                <img src={image} alt="item" 
-                                className="max-w-20 rounded-2xl"    
-                                onClick={ () => handleImage(image)}
-                            />
-                            )
-                        })
-                    }
-                            {/* <img src="https://images.pexels.com/photos/1502216/pexels-photo-1502216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="item" 
-                                className="max-w-20 rounded-2xl"    
-                            />
-                            <img src="https://images.pexels.com/photos/1502216/pexels-photo-1502216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="item" 
-                                className="max-w-20 rounded-2xl"
-                            />
-                            <img src="https://images.pexels.com/photos/1502216/pexels-photo-1502216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="item" 
-                                className="max-w-20 rounded-2xl"
-                            /> */}
-                </div>
-
-                <div className="w-1/5 flex">
-                    <button 
-                    className="mt-10 w-full h-8 bg-primary/70 hover:bg-primary rounded-2xl py-2 text-black">Go</button>
-                </div>
-
-             </div>
 
         </div>
 
