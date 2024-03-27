8561ca05435ddf4bd68d7a0bb0b8917cdefd4048
BBrendaBaumann <brenbaumann40@gmail.com>
2024-03-26 21:11:50 -0300

    sale responsive finish

--- Client/src/components/sale/Sale.jsx
+++ Client/src/components/sale/Sale.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import SaleOneSlider from "../Oneslider/SaleOneSlider";
 
-
+//pl-44
 
 const Sale = () => {
 
     return(
-        <div className="mt-20 mx-44 ml-44 pl-44 font-RedHat flex flex-col align-center lg:flex-row space-x-5 border-l-500">
+        <div className="lg:w-5/6 lg:mt-20 mx-auto font-RedHat flex flex-col align-center md:flex-row space-x-5 border-l-500 p-4 justify-center">
 
-        <div className="text-center p-10 border-8 border-primary border-double rounded-lg flex flex-col justify-center  w-2/6 h-3/4">
+        <div className="text-center p-10 border-8 border-primary border-double rounded-lg flex flex-col justify-center  w-full h-auto md:w-2/6">
             <p className="text-base text-black-600/50 font-bold italic ">SUMMER</p>
             <h2 className=" text-6xl tracking-[.25em] font-bold text-gray-800 my-40">SALE</h2>
             <p>ENDS 20.03.2024</p>
