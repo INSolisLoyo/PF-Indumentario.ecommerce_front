7dd4deba995e04fcb64d76de2da4bbe79080f77f
Nohemi Solis <mimisolisloyo@gmail.com>
2024-04-03 11:50:49 -0600

    Diseño inicial terminado

--- Client/src/components/Login/Login.jsx
+++ Client/src/components/Login/Login.jsx
@@ -36,8 +36,10 @@ export default function Login({ onClose }) {
           email: user,
           password: password
         })
-      }).then( res => res.json()).then( (cred) => 
-        console.log(cred)
+      }).then( res => res.json()).then( (cred) => {
+        document.cookie = `token=${cred.token}; max-age=${60 * 60}; path=/; samesite=strict`
+        console.log(document.cookie)
+      }
       )
       setUser('');
       setPassword('');
@@ -58,79 +60,86 @@ export default function Login({ onClose }) {
   }
 
   return (
-    <div className="absolute top-16 right-2 p-4 border-none rounded-lg shadow w-[350px] h-[350px] text-center shadow-slate-500 font-RedHat bg-white max-lg:right-2 max-lg:w-[320px] ">
-      <p ref={errRef} aria-live="assertive">{errMsg}</p>
-      <h2 className="text-black text-xl font-bold ">Login</h2>
-      <div className="bg-black w-[250px] p-[0.8px] m-auto mt-2 mb-3 "></div>
-      <form className=" pb-4 " onSubmit={handleSubmit}>
-        <label
-          className="block  text-left ml-12 uppercase text-[13px] tracking-[1px]  "
-          htmlFor=""
-        >
-          Username
-        </label>
-        <input
-          className="border-none rounded-lg bg-primary/20 mb-5 text-black outline-none p-1 w-[220px] "
-          type="text"
-          id="username"
-          ref={userRef}
-          autoComplete="off"
-          onChange={(e) => setUser(e.target.value)}
-          value={user}
-          required
-        />
+    <div className="absolute right-0 top-0 border-none rounded-lg shadow shadow-slate-500 font-RedHat bg-white md:w-1/3 md:h-svh">
 
-        <label
-          className="block text-[13px] text-left ml-12 uppercase tracking-[1px]  "
-          htmlFor=""
-        >
-          Password
-        </label>
-        <input
-          className="border-none rounded-lg bg-primary/20 mb-2 text-black outline-none p-1 w-[220px] "
-          type="password"
-          id="password"
-          onChange={ (e) => setPassword(e.target.value)}
-          value={password}
-          required
-        />
-        <button
-          className="bg-primary/50 p-2 w-[80px] rounded-xl mb-4 uppercase "
-        >
-          GO
-        </button>
-      </form>
+      <div className="lg:p-4 flex flex-col">
 
-      <div className="login-socials items-center mb-[8px]  ">
-        <a href="http://localhost:3001/auth/facebook">
-          <button className="w-[60px] mr-[5px] shadow-lg shadow-gray-300 p-1 rounded-lg bg-blue-700  ">
-            <img
-              className=" w-[20px] m-auto "
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/768px-Facebook_Logo_%282019%29.png"
-              alt=""
-            />
+        <div className="text-black flex flex-col items-end text-2xl">
+          <p onClick={ () => onClose()} className="cursor-pointer">X</p>
+          <div className="w-full text-center">
+            <h2 className="block text-center">Log In</h2>
+          </div>
+        </div>
+
+        <div className="bg-black w-11/12 p-[0.4px] m-auto mt-2 mb-3"></div>
+
+        <form className=" text-center flex flex-col items-center md:mt-6" onSubmit={handleSubmit}>
+
+          <label className="text-xl" htmlFor="username">
+            Email
+          </label>
+          <input
+            className=" w-full p-2 md:mt-4 border-none rounded-lg bg-primary/20 mb-5 text-black outline-none  "
+            type="text"
+            id="username"
+            ref={userRef}
+            autoComplete="off"
+            placeholder="user@account.com"
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            required
+          />
+
+          <label className="text-xl md:mt-6" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="w-full p-2 md:mt-4 border-none rounded-lg bg-primary/20 mb-5 text-black outline-none "
+            type="password"
+            id="password"
+            onChange={ (e) => setPassword(e.target.value)}
+            value={password}
+            required
+          />
+          <button
+            className="bg-primary/50 hover:bg-primary p-2 w-28 md:mt-6 rounded-xl mb-4"
+          >
+            Log In
           </button>
-        </a>
+        </form>
 
-        <a href="http://localhost:3001/google/callback">
-          <button className="w-[60px] ml-[5px] shadow-lg shadow-gray-300 rounded-lg  p-1 border border-slate-200  ">
-            <img
-              className="w-[20px] m-auto "
-              src="https://cdn-icons-png.flaticon.com/512/300/300221.png"
-              alt="Google Icon"
-            />
-          </button>
-        </a>
+        <div className="login-socials flex justify-center md:gap-4 items-center md:mt-6">
+          <a href="http://localhost:3001/auth/facebook">
+            <button className="w-[60px] mr-[5px] shadow-lg shadow-gray-300 p-1 rounded-lg bg-blue-700  ">
+              <img
+                className=" w-[20px] m-auto "
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/768px-Facebook_Logo_%282019%29.png"
+                alt=""
+              />
+            </button>
+          </a>
+
+          <a href="http://localhost:3001/google/callback">
+            <button className="w-[60px] ml-[5px] shadow-lg shadow-gray-300 rounded-lg  p-1 border border-slate-200  ">
+              <img
+                className="w-[20px] m-auto "
+                src="https://cdn-icons-png.flaticon.com/512/300/300221.png"
+                alt="Google Icon"
+              />
+            </button>
+          </a>
+        </div>
+
+        <div className="md:mt-6 text-center">
+          <p className="uppercase text-[12px]">
+            Do u need an acount?{" "}
+            <a className=" text-blue-700 uppercase " href="/register">
+              Click here
+            </a>{" "}
+          </p>
+        </div>
       </div>
 
-      <div className="need-acount">
-        <p className="uppercase text-[12px]">
-          Do u need an acount?{" "}
-          <a className=" text-blue-700 uppercase " href="/register">
-            Click here
-          </a>{" "}
-        </p>
-      </div>
     </div>
   );
 }
