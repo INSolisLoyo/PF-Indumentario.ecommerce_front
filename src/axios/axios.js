1cd77a90e9cddd7af05ea7b9b05b11bcfd2dac69
JMurilloCortes <jealmuco@gmail.com>
2024-04-09 19:15:52 -0500

    Acondicionando Rama para Sprint 3

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const BASE_URL = "https://ecommerce-becomfree.onrender.com";
 
 export default axios.create({
     baseURL: BASE_URL
