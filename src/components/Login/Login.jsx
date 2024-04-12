4be26f3f2f507f63c0c3cf1dd5ee66d7e0b800a7
Nohemi Solis <mimisolisloyo@gmail.com>
2024-04-11 23:19:52 -0600

    Estilo terminado, falta mejorar el responsive

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,8 +2,13 @@ import useStore from "../GlobalStoreZustand/GlobalStoreZustand";
 import React, { useState, useRef, useEffect } from "react";
 import { useNavigate, useLocation } from 'react-router-dom';
 import {jwtDecode} from 'jwt-decode';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faXmark, faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import axios from "../../axios/axios";
 import AuthTerceros from "../AuthTerceros/AuthTerceros";
+import SesionSwitch from "./Switch";
+import PopoverInfo from "./PopoverInfo";
 
 
 const LOGIN_URL = '/login';
@@ -12,167 +17,91 @@ const PWD_REGEX = /.+/;
 
 export default function Login({ onClose }) {
 
-  const setCurrentUser = useStore((state) => state.setCurrentUser )
-  const setRegisteredUser = useStore((state) => state.setRegisteredUser)
-  
-  const navigate = useNavigate();
-  const location = useLocation();
-  const origin = location.state?.from?.pathname || "/";
+  const [showPassword, setShowPassword] = useState(false);
 
-  const userRef = useRef();
-  const errRef = useRef();
-
-  const [user, setUser] = useState('')
-  const [validUser, setValidUser] = useState(false);
-  const [password, setPassword] = useState('');
-  const [validPassword, setValidPassword] = useState(false);
-  const [errMsg, setErrMsg] = useState('');
-  
-  useEffect(() => {
-    userRef.current.focus();
-  }, [])
-
-  useEffect(() => {
-    setValidUser(USER_REGEX.test(user));
-    if(!validUser){
-      setErrMsg('Enter a valid email')
-    }
-  }, [user])
-
-  useEffect(() => {
-      setValidPassword(PWD_REGEX.test(password));
-      if(!validPassword){
-        setErrMsg('Enter a valid password')
-      }
-  }, [password])
-
-  useEffect(() => {
-    setErrMsg('');
-  }, [user, password])
-
-
-  const handleSubmit = async (e) => {
-
-    e.preventDefault();
-
-    const v1 = USER_REGEX.test(user);
-    const v2 = PWD_REGEX.test(password);
-    if (!v1 || !v2) {
-        setErrMsg("Invalid Entry");
-        return;
-    }
-
-    try {
-
-      axios.post(LOGIN_URL, {
-      email: user,
-      password: password
-    }, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then((res) => {
-      const cred = res.data;
-      document.cookie = `token=${cred.token}; max-age=${60 * 60}; path=/; samesite=strict`;
-      const { userId, userName, userLastname, userBirthdate, userEmail, userPassword, isAdmin, isActive } = jwtDecode(cred.token);
-      
-      const newUser = {
-        id: userId,
-        name: userName,
-        lastname: userLastname,
-        dob: userBirthdate,
-        email: userEmail,
-        password: userPassword,
-        isAdmin: isAdmin,
-        isActive: isActive
-      };
-      setCurrentUser(newUser);
-      setRegisteredUser(true);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-      
-      onClose();
-      navigate(origin, { replace: true });
-     
-    } catch (error) {
-      if (!err?.response) {
-        setErrMsg('No Server Response');
-      } else if (err.response?.status === 400) {
-          setErrMsg('Missing Username or Password');
-      } else if (err.response?.status === 401) {
-          setErrMsg('Unauthorized');
-      } else {
-          setErrMsg('Login Failed');
-      }
-      errRef.current.focus();
-    }
-  }
-
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   
   return (
-    <div className="absolute right-0 top-0 border-none rounded-lg shadow shadow-slate-500 font-RedHat bg-white md:w-1/3 md:h-svh">
+    <div className="absolute right-2 top-2 w-11/12 h-auto md:right-4 md:top-4 border-none rounded-lg shadow shadow-slate-500 font-RedHat bg-white md:w-1/3 ">
 
-      <div className="lg:p-4 flex flex-col">
+      <div className="w-full p-2 md:p-4 flex flex-col gap-2 md:gap-4">
 
-        <div className="text-black flex flex-col items-end text-2xl">
-          <p onClick={ () => onClose()} className="cursor-pointer">X</p>
-          <div className="w-full text-center">
-            <h2 className="block text-center">Log In</h2>
-          </div>
+        {/* Ícono de x */}
+        <div className="w-10/12 mx-auto flex justify-end">
+
+          <FontAwesomeIcon icon={faXmark} size="xl" className="text-[#3c1613] cursor-pointer hover:text-primary/80" onClick={onClose}/>
+
         </div>
 
-        <div className="bg-black w-11/12 p-[0.4px] m-auto mt-2 mb-3"></div>
+        {/* Contenedor General  */}
+        <div className="w-9/12 mx-auto flex flex-col justify-center gap-6">
 
-        <form className=" text-center flex flex-col items-center md:mt-6" onSubmit={handleSubmit}>
+          {/* Título */}
+          <p className="text-4xl text-center text-slate-800 font-bold">Log in</p>
 
-          <label className="text-xl" htmlFor="username">
-            Email
-          </label>
-          <input
-            className=" w-full p-2 md:mt-4 border-none rounded-lg bg-primary/20 mb-5 text-black outline-none  "
-            type="text"
-            id="username"
-            ref={userRef}
-            autoComplete="off"
-            placeholder="user@account.com"
-            onChange={(e) => setUser(e.target.value)}
-            value={user}
-            required
-          />
-          {!validUser && <p ref={errRef} className="text-lg text-red-600 mt-4">{errMsg}</p>}
-
-          <label className="text-xl md:mt-6" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="w-full p-2 md:mt-4 border-none rounded-lg bg-primary/20 mb-5 text-black outline-none "
-            type="password"
-            id="password"
-            onChange={ (e) => setPassword(e.target.value)}
-            value={password}
-            required
-          />
-          {!validPassword && <p ref={errRef} className="text-lg text-red-600 mt-4">{errMsg}</p>}
-          <button
-            className="bg-primary/50 hover:bg-primary p-2 w-28 md:mt-6 rounded-xl mb-4"
-          >
-            Log In
+          {/* Autenticación de terceros */}
+          
+          <button className="w-full py-2 border border-gray-300 rounded-xl flex gap-4 justify-center items-center hover:bg-[#fae8e6]">
+            <FontAwesomeIcon icon={faGoogle} size="xl" className=" text-slate-800"/> <span>Log in with Google</span>
           </button>
-        </form>
 
-        <AuthTerceros onClose={onClose}/>
+          {/* OR */}
+          <div className="relative">
+            <div className="h-[2px] bg-gray-300"></div>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-full w-12 flex items-center justify-center">
+             <p>OR</p>
+            </div>
+          </div>
+          
+          {/* Inputs */}
+          <div className="flex flex-col gap-4">
 
-        <div className="md:mt-6 text-center">
-          <p className="uppercase text-[12px]">
-            Do u need an acount?{" "}
-            <a className=" text-blue-700 uppercase " href="/register">
-              Click here
-            </a>{" "}
+              <input className="w-full py-2 px-4 border border-gray-300 rounded-xl flex gap-4 justify-center items-center focus:border-[#f6d5d2]" 
+                placeholder="Enter your email" type="email"
+              />
+              
+              <div className="relative">
+              
+                <input className="w-full py-2 px-4 border border-gray-300 rounded-xl flex gap-4 justify-center items-center" 
+                  placeholder="Enter your password" type={showPassword ? 'text' : 'password'}                
+                />
+                
+                <div className="absolute inset-y-0 right-2 flex items-center pr-4">
+                  {showPassword ? <FontAwesomeIcon icon={faEye} onClick={togglePasswordVisibility}/> : <FontAwesomeIcon icon={faEyeSlash} onClick={togglePasswordVisibility}/>}
+                </div>
+
+              </div>
+
+    
+
+              {/* Keep sesion open? */}
+              <div className="flex gap-2 items-center">
+                <SesionSwitch/>
+                <p>Keep the session open</p>
+                <PopoverInfo/>
+              </div>
+
+              <button className="w-full py-2 border border-gray-300 bg-[#fae8e6] hover:bg-primary rounded-xl flex gap-4 justify-center items-center">
+                Log in
+              </button>
+
+          </div>
+
+          { /* Forget password */}
+          <p className="text-center">
+            <a href="#" className="underline">Forgot your password?</a>
           </p>
+
+          {/* Create an account*/}
+          <button className="w-full py-2 border border-gray-300 bg-[#fae8e6] hover:bg-primary rounded-xl flex     justify-center items-center">
+                Create an account
+          </button>
+
         </div>
+
+
       </div>
 
     </div>
---
+++ src/components/Login/Switch.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { Switch } from '@headlessui/react'
+
+export default function SesionSwitch() {
+
+  const [enabled, setEnabled] = useState(false)
+
+  return (
+    
+    <Switch
+    checked={enabled}
+    onChange={setEnabled}
+    className={`${enabled ? 'bg-primary' : 'bg-[#fae8e6]'}
+       relative inline-flex items-center h-6 w-12 shrink-0 cursor-pointer rounded-full border-2 border-[#f6d5d2] transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white/75`}
+  >
+    <span
+      aria-hidden="true"
+      className={`${enabled ? 'translate-x-6' : 'translate-x-0'}
+        pointer-events-none inline-block h-4 w-4 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+    />
+  </Switch>
+  
+    
+  )
+}
