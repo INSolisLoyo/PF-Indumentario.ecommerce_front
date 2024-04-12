8b7b545a315812cd1113e59591d99bbe90081198
JMurilloCortes <jealmuco@gmail.com>
2024-04-12 14:19:37 -0500

    Ultimos ajustes para merge

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -191,13 +191,13 @@ const Footer = () => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
-          class="w-6 h-6 text-white  "
+          className="w-6 h-6 text-white  "
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18"
           />
         </svg>
