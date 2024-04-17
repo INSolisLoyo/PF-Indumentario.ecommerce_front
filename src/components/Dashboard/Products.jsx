cbdf4bf4ac2928f034a50f991f6bd269502e03a3
Nohemi Solis <mimisolisloyo@gmail.com>
2024-04-16 21:46:25 -0600

    Productos renderizados correctamente

--- src/components/Dashboard/Products.jsx
+++ src/components/Dashboard/Products.jsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import Select from "react-select";
+import axios from '../../axios/axios';
+
 
 export default function Products() {
 
@@ -13,10 +16,66 @@ export default function Products() {
 
   }
 
+  const handleStatusChange = () => {
+
+  }
+
+  const fetchProducts = async () => {
+
+    const { data } = await axios.post('/product', {
+      name: '',
+      gender: undefined,
+      minPrice: 0,
+      maxPrice: 158,
+      material: [],
+      colour: [],
+      orderType: '',
+      order: '',
+      category: undefined,
+      pageNumber: 1,
+      productLimit: 160
+    })
+
+    setProducts(data);
+
+  }
+
+  useEffect(() => {
+
+    fetchProducts();
+
+  }, [])
+
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.selectProps.menuIsOpen
+        ? "#f3f4f6"
+        : state.selectProps.value && state.selectProps.value.value
+        ? "#d1fae5"
+        : "#fee2e2",
+      borderColor: state.selectProps.menuIsOpen ? "#7c3aed" : "#e5e7eb",
+      "&:hover": {
+        borderColor: state.selectProps.menuIsOpen
+          ? "#7c3aed"
+          : state.selectProps.value && state.selectProps.value.value
+          ? "#d1fae5"
+          : "#fee2e2",
+      },
+    }),
+    singleValue: (provided, state) => ({
+      ...provided,
+      color:
+        state.selectProps.value && state.selectProps.value.value
+          ? "#059669"
+          : "#9b2c2c",
+    }),
+  };
+
   return (
 
     // Div principal
-    <div className="overflow-x-auto font-RedHat ">
+    <div className="w-full h-auto overflow-x-auto font-RedHat ">
 
       <h2 className="text-center text-xl pb-5 ">Products</h2>
 
@@ -55,6 +114,10 @@ export default function Products() {
                   Name
                 </th>
 
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Status
+                </th>
+
                 <th className="px-6 py-5 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 </th>
 
@@ -67,48 +130,75 @@ export default function Products() {
 
             <tbody className='bg-white divide-y divide-gray-200'>
 
-              <tr className="bg-white shadow-md border-b border-slate-300">
+              {
+                products?.map( (product) => {
 
-                <td className="px-6 py-4 whitespace-nowrap border-b border-slate-300">
-                  Patito
-                </td>
+                  return (
+                    
+                    <tr className="bg-white shadow-md border-b border-slate-300">
 
-                <td className="px-6 py-4 whitespace-nowrap border-b border-slate-300">
-                  123-456-789-ABC
-                </td>
+                      <td className="px-2 py-4 whitespace-nowrap border-b border-slate-300">
+                        {product.name}
+                      </td>
 
-                <td className="px-6 py-4 whitespace-nowrap border-b border-slate-300">
-                  
-                  <button className="bg-green-600 w-[90px] flex justify-center text-center gap-1 items-center text-white p-2 rounded-md ">Update</button>
+                      <td className="px-2 py-4 whitespace-nowrap border-b border-slate-300">
+                        {product.id}
+                      </td>
 
-                </td>
+                      <td className="px-2 py-4 whitespace-nowrap border-b border-slate-300 ">
+                          <Select
+                            defaultValue={{
+                              value: product.isActive,
+                              label: product.isActive ? "Active" : "Inactive",
+                            }}
+                            options={[
+                              { value: true, label: "Active" },
+                              { value: false, label: "Inactive" },
+                            ]}
+                            onChange={(selectedOption) =>
+                              handleStatusChange(product.id, selectedOption.value)
+                            }
+                            styles={customStyles}
+                          />
+                      </td>
 
-                <td className="px-6 py-4 whitespace-nowrap border-b border-slate-300">
-                  
-                  <button className="bg-red-600 w-[90px] flex justify-center text-center gap-1 items-center text-white p-2 rounded-md ">
+                      <td className="px-6 py-4 whitespace-nowrap border-b border-slate-300">
+                        
+                        <button className="bg-green-600 w-[90px] flex justify-center text-center gap-1 items-center text-white p-2 rounded-md" onClick={handleUpdate}>Update</button>
 
-                    Delete
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="w-4 h-4"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                      />
-                    </svg>
+                      </td>
+
+                      <td className="px-6 py-4 whitespace-nowrap border-b border-slate-300">
+                        
+                        <button className="bg-red-600 w-[90px] flex justify-center text-center gap-1 items-center text-white p-2 rounded-md" onClick={handleDelete}>
+
+                          Delete
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="w-4 h-4"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                            />
+                          </svg>
 
 
-                  </button>
+                        </button>
 
-                </td>
+                      </td>
 
-              </tr>
+                    </tr>
+
+                  )
+                })
+              }
+
 
             </tbody>
 
